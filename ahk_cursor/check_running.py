40621f56538a8782b89c6b37477856d0c7472e7d
Maple <wjxa20152015@gmail.com>
2025-08-09 22:11:19 +0800

    support different executable names

--- ahk_cursor/check_running.py
+++ ahk_cursor/check_running.py
@@ -2,11 +2,11 @@ from ahk_cursor.api import GetProcessByName
 from ahk_cursor.logger import logger
 
 
-def check_running():
-    ret = GetProcessByName("BGI.exe")
+def check_running(executable_name="BGI.exe"):
+    ret = GetProcessByName(executable_name)
     if not ret:
-        logger.info("BGI not running")
+        logger.info(f"{executable_name} not running")
         return -1
     else:
-        logger.info("BGI running")
+        logger.info(f"{executable_name} running")
         return ret
